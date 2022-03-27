71b407fe21a75802ef4c34db18efd6b4c3c03379
Hassan Gaber <97131719+Hassan667@users.noreply.github.com>
2022-03-27 11:09:42 +0200

    new

---
+++ News-System api/src/models/news.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+    // const reporters = require('./reporters')
+
+const newsScehma = new mongoose.Schema({
+
+        title: {
+            type: String,
+            trim: true,
+            required: true
+
+        },
+        description: {
+            type: String,
+            trim: true
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'reporters'
+        }
+    },
+
+
+    {
+        timestamps: true
+    }
+
+
+)
+
+const News = mongoose.model('News', newsScehma)
+
+module.exports = News
